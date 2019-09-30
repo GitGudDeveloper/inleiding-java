b08e93cdc75002dc360c60c4fbb7f7407080d0b8
Quincy van Kempen <2113342@talnet.nl>
2019-09-30 10:27:03 +0200

    hoofdstuk 10 opdracht 2

--- src/h10/opdracht2.java
+++ src/h10/opdracht2.java
@@ -6,51 +6,68 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class opdracht2 extends Applet {
-    int getal;
+    boolean startgetal;
+    int ingevoerdgetal;
     int maxlimiet;
     int minlimiet;
     TextField tekstvak;
-    TextField tekstvak2;
     Label label;
     String tekst;
+    String tekst2;
 
     public void init() {
         tekstvak = new TextField("", 5);
         tekstvak.addActionListener(new VakListener());
         tekst = "";
+        tekst2 = "";
 
         label = new Label("het hoogste cijfer wordt onthouden");
 
         add(label);
         add(tekstvak);
+        startgetal = true;
+
     }
 
+
     public void paint(Graphics g) {
-        g.drawString(tekst, 50, 45);
+        g.drawString("maximum: " + tekst, 50, 50);
+        g.drawString("minimum: " + tekst2, 50, 100);
     }
 
     class VakListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            String s;
-
-            s = tekstvak.getText();
-            getal = Integer.parseInt(s);
-            if (getal > maxlimiet) {
-                maxlimiet = getal;
+            String ingevoerdgetalString = tekstvak.getText();
+            ingevoerdgetal = Integer.parseInt(ingevoerdgetalString);
+            if (startgetal) {
+                startgetal = false;
+                maxlimiet = ingevoerdgetal;
+                minlimiet = ingevoerdgetal;
                 tekst = String.valueOf(maxlimiet);
-            } repaint();
-                {
+                tekst2 = String.valueOf(minlimiet);
+                repaint();
+            } else {
+                if (ingevoerdgetal > maxlimiet) {
+                    maxlimiet = ingevoerdgetal;
+                    tekst = String.valueOf(maxlimiet);
+                    repaint();
 
 
-                s = tekstvak2.getText();
-                getal = Integer.parseInt(s);
-                if (getal < minlimiet) {
-                    minlimiet = getal;
-                    tekst = String.valueOf(minlimiet);
-                } repaint();
-                
+                }else if (ingevoerdgetal < minlimiet) {
+                minlimiet = ingevoerdgetal;
+                tekst2 = String.valueOf(minlimiet);
+                repaint();
+            } else {repaint();}
+
+
+
+
+
+
             }
         }
     }
-
 }
+
+
+
