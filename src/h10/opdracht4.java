f04ad1d8f7dcb1539bd2a9a0f5bdb08586c93aa9
Quincy van Kempen <2113342@talnet.nl>
2019-10-01 11:30:12 +0200

    hoofdstuk 10 opdracht 4

---
+++ src/h10/opdracht4.java
@@ -0,0 +1,97 @@
+package h10;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class opdracht4 extends Applet {
+    TextField tekstvak;
+    TextField tekstvak2;
+    boolean startgetal;
+    int maandnummer;
+    int jaartal;
+    String datum;
+    String schikkeljaar;
+    Label label;
+    Label label2;
+
+
+    public void init() {
+        setSize(500, 500);
+        tekstvak = new TextField(5);
+        tekstvak.addActionListener(new Vaklistener());
+        label = new Label("type een jaartal en druk op enter");
+        add(label);
+        add(tekstvak);
+        tekstvak2 = new TextField(5);
+        tekstvak2.addActionListener(new tekstvak2listener());
+        label2 = new Label("type een maandnummer en druk op enter.");
+        add(label2);
+        add(tekstvak2);
+
+        schikkeljaar = "";
+        datum = "";
+
+
+
+    }
+
+
+    public void paint(Graphics g) {
+        g.drawString(datum, 30, 200);
+        g.drawString(schikkeljaar, 30, 180);
+
+    }
+
+    private class Vaklistener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String jaartalString = tekstvak.getText();
+            jaartal = Integer.parseInt(jaartalString);
+            if ((jaartal % 4 == 0 && !(jaartal % 100 == 0)) ||
+                    jaartal % 400 == 0 ) {
+                schikkeljaar = "jaar";
+
+            }
+
+        }
+    }
+
+    private class tekstvak2listener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String maandnummerString = tekstvak2.getText();
+            maandnummer = Integer.parseInt(maandnummerString);
+            if (startgetal) {
+                startgetal = false;
+            } else {
+                if (maandnummer == 1) {
+                    datum = "de maand Januari heeft 31 dagen";
+                } else if (maandnummer == 2) {
+                    datum = " de maand Februari heeft 29 dagen";
+                } else if (maandnummer == 3) {
+                    datum = " de maand Maart heeft 31 ";
+                } else if(maandnummer == 4) {
+                    datum = " de maand April heeft 30 dagen";
+                } else if (maandnummer == 5) {
+                    datum = " de maand Mei heeft 31 dagen";
+                } else if (maandnummer == 6) {
+                    datum = " de maand Juni 30 dagen";
+                } else if (maandnummer == 7) {
+                    datum = " de maand Juli heeft 31 dagen";
+                } else if (maandnummer == 8) {
+                    datum = " de maand Augustus heeft 31 dagen";
+                } else if (maandnummer == 9) {
+                    datum = " de maand September heeft 30 dagen";
+                } else if (maandnummer == 10) {
+                    datum = " de maand Oktober heeft 31 dagen";
+                } else if (maandnummer == 11) {
+                    datum = " de maand November heeft 30 dagen";
+                } else if (maandnummer == 12) {
+                    datum = " de maand December heeft 31 dagen";
+                } else
+                    datum = "";
+            } repaint();
+
+        }
+    }
+}
