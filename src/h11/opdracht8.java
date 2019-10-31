a9091c54ddaf2d68d1307c02b75ec036675337c9
Quincy van Kempen <2113342@talnet.nl>
2019-10-31 08:42:38 +0100

    hoofdstuk 11 opdracht 1

--- src/h11/opdracht8.java
+++ src/h11/opdracht8.java
@@ -10,13 +10,11 @@ public class opdracht8 extends Applet {
     }
 
     public void paint(Graphics g) {
-        int x = 495;
-        int y = 495;
+        int x = 5;
+        int y = 5;
         int width = 10;
         int height = 10;
         for (int c = 0; c<100; c++) {
-            x-=5;
-            y-=5;
             width+=10;
             height+=10;
             g.drawOval(x, y, width, height);
